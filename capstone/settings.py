4f2beee35bf7cca50c89e1f55615a849e7a17daf
nvsriram <nuranisri01@gmail.com>
2022-10-18 05:33:23 -0400

    Added STATIC_ROOT to settings.py

--- capstone/settings.py
+++ capstone/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+from email.mime import base
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -115,7 +117,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "static/"
 ASGI_APPLICATION = "capstone.routing.application"
 
