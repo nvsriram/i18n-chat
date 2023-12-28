b17466764e9561b4ddc61ba9e818e4130de38ff2
nvsriram <nuranisri01@gmail.com>
2023-12-29 00:24:11 +0400

    Refactor FormContext

---
+++ react/chat/src/contexts/FormContext.tsx
@@ -0,0 +1,6 @@
+import { createContext } from "react";
+import { IFormContext } from "../types";
+
+export const FormContext = createContext<IFormContext | null>(null);
+
+export const FormProvider = FormContext.Provider;
