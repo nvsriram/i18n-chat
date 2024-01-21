90d744caee9bbda483f159ff2597b35f10899eef
nvsriram <nuranisri01@gmail.com>
2024-01-21 14:40:38 -0500

    Run prettier on config files

--- react/chat/.eslintrc.cjs
+++ react/chat/.eslintrc.cjs
@@ -2,25 +2,25 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parser: "@typescript-eslint/parser",
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
   plugins: [
-    "react-refresh",
-    "@typescript-eslint",
-    "react",
-    "simple-import-sort",
-    "import",
+    'react-refresh',
+    '@typescript-eslint',
+    'react',
+    'simple-import-sort',
+    'import',
   ],
   rules: {
-    "react-refresh/only-export-components": [
-      "warn",
+    'react-refresh/only-export-components': [
+      'warn',
       { allowConstantExport: true },
     ],
-    "react/jsx-sort-props": [
+    'react/jsx-sort-props': [
       2,
       {
         callbacksLast: true,
@@ -29,30 +29,30 @@ module.exports = {
         reservedFirst: true,
       },
     ],
-    "import/order": [
-      "warn",
+    'import/order': [
+      'warn',
       {
-        alphabetize: { order: "asc", caseInsensitive: true },
+        alphabetize: { order: 'asc', caseInsensitive: true },
         pathGroups: [
           {
-            pattern: "@/**",
-            group: "external",
-            position: "after",
+            pattern: '@/**',
+            group: 'external',
+            position: 'after',
           },
         ],
-        "newlines-between": "always",
+        'newlines-between': 'always',
       },
     ],
-    "array-callback-return": [
-      "error",
+    'array-callback-return': [
+      'error',
       {
         checkForEach: true,
       },
     ],
-    "simple-import-sort/exports": "error",
-    "import/first": "error",
-    "import/newline-after-import": "error",
-    "import/no-duplicates": "error",
-    "import/no-anonymous-default-export": "warn",
+    'simple-import-sort/exports': 'error',
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+    'import/no-anonymous-default-export': 'warn',
   },
 };
