53c65325be0480957084def06c7d71519857c8e1
nvsriram <nuranisri01@gmail.com>
2024-01-31 00:07:33 -0500

    Run prettier

--- react/chat/src/components/TextInput.tsx
+++ react/chat/src/components/TextInput.tsx
@@ -1,14 +1,19 @@
-import TextField, { TextFieldProps } from "@mui/material/TextField";
-import { useEffect } from "react";
+import TextField, { TextFieldProps } from '@mui/material/TextField';
+import { useEffect } from 'react';
 
-import { useFormContext } from "@/hooks";
+import { useFormContext } from '@/hooks';
 
 type ITextInput = TextFieldProps & {
   name: string;
   removeHelperText?: boolean;
 };
 
-export const TextInput = ({ name, removeHelperText, sx, ...props }: ITextInput) => {
+export const TextInput = ({
+  name,
+  removeHelperText,
+  sx,
+  ...props
+}: ITextInput) => {
   const formContext = useFormContext();
 
   useEffect(() => {
@@ -25,16 +30,16 @@ export const TextInput = ({ name, removeHelperText, sx, ...props }: ITextInput) 
       }
       helperText={
         removeHelperText!
-          ? ""
+          ? ''
           : name in formContext.inputs && formContext.inputs[name].invalid
-          ? formContext.inputs[name].invalidMsg
-          : " "
+            ? formContext.inputs[name].invalidMsg
+            : ' '
       }
       margin="normal"
       name={name}
       required={!removeHelperText}
       sx={{ mt: 0, ...sx }}
-      value={name in formContext.inputs ? formContext.inputs[name].value : ""}
+      value={name in formContext.inputs ? formContext.inputs[name].value : ''}
       onChange={formContext.onChange}
       {...props}
     />
