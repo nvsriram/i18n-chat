b8ba946bd262a51f2649849f620d86b7e7591247
nvsriram <nuranisri01@gmail.com>
2024-02-10 18:54:48 -0500

    Remove django files from python folder

--- django/chat/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import ChatUser, Room, Message
-
-# Register your models here.
-admin.site.register(ChatUser)
-admin.site.register(Room)
-admin.site.register(Message)
